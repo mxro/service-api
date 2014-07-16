ac4751d98a0f1d707af3a049610f766fa989fe3d
mxro <mxro@nowhere.com>
2014-07-16 15:49:22 +1200

    Refactoring Appjangle Server (nxAppCloudNode) to make it more portable and usable for tests Autocommit Wed Jul 16 15:49:22 NZST 2014

--- src/main/java/de/mxro/service/ServiceRegistry.java
+++ src/main/java/de/mxro/service/ServiceRegistry.java
@@ -4,11 +4,13 @@ public interface ServiceRegistry {
 
 	/**
 	 * Seeks first match in registry of service which provides the provided
-	 * interface of type (e.g. service can be cast to clazz).
+	 * interface of type clazz (e.g. service can be cast to clazz).
 	 * 
 	 * @param clazz
 	 * @return
 	 */
 	public Service get(Class<?> clazz);
 
+	public void register(Service service);
+	
 }
