13c39742399b0923876261dd85e990f9e11ebca3
mxro <mxro@nowhere.com>
2014-07-16 15:56:22 +1200

    Refactoring Appjangle Server (nxAppCloudNode) to make it more portable and usable for tests Autocommit Wed Jul 16 15:56:22 NZST 2014

--- src/main/java/de/mxro/service/Services.java
+++ src/main/java/de/mxro/service/Services.java
@@ -2,9 +2,6 @@ package de.mxro.service;
 
 public class Services {
 
-	public static <ServiceType extends Service> void start(de.mxro.factories.Factory<ServiceType, ?, ?> factory, de.mxro.service.callbacks.StartCallback callback) {
-		
-		
-	}
+	
 	
 }
