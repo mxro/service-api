b8ddfc8e0ba2cde67554ee962d29c5fd058ecae3
mxro <mxro@nowhere.com>
2014-07-28 13:04:19 +1000

    Improving core services and interfaces Autocommit Mon Jul 28 13:04:19 EST 2014

--- src/main/java/de/mxro/service/ServiceRegistry.java
+++ src/main/java/de/mxro/service/ServiceRegistry.java
@@ -5,14 +5,21 @@ import de.mxro.service.callbacks.GetServiceCallback;
 public interface ServiceRegistry {
 
 	/**
-	 * Seeks first match in registry of service which provides the provided
+	 * <p>Seeks first match in registry of service which provides the provided
 	 * interface of type clazz (e.g. service can be cast to clazz).
+	 * <p>Subscribe as a user to this service
 	 * 
 	 * @param clazz
 	 * @return
 	 */
 	public <InterfaceType> void subscribe(Class<InterfaceType> clazz, GetServiceCallback<InterfaceType> callback);
 
+	/**
+	 * Release a subscription for this service.
+	 * @param service
+	 */
+	public void unsubscribe(Service service);
+	
 	public void register(Service service);
 	
 
