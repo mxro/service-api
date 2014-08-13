173d7ff8c517a6f3ce653f88b56d1f2827dc7a77
mxro <mxro@nowhere.com>
2014-08-13 17:12:06 +1000

    Improving way services are stopped. Adding utility to look for pending requests Autocommit Wed Aug 13 17:12:06 EST 2014

--- src/main/java/de/mxro/service/jre/ServiceJre.java
+++ src/main/java/de/mxro/service/jre/ServiceJre.java
@@ -1,12 +1,18 @@
 package de.mxro.service.jre;
 
 import de.mxro.service.jre.internal.ServiceActivityMonitorImpl;
+import de.mxro.service.jre.internal.ShutdownHelperImpl;
 import de.mxro.service.utils.ActivityMonitor;
+import de.mxro.service.utils.ShutdownHelper;
 
 public class ServiceJre {
 
 	public static final ActivityMonitor createActivityMonitor() {
 		return new ServiceActivityMonitorImpl();
+	}
+	
+	public static final ShutdownHelper createShutdownHelper(ActivityMonitor activityMonitor) {
+		return new ShutdownHelperImpl(activityMonitor);
 	}
 
 }
--- src/main/java/de/mxro/service/jre/internal/ShutdownHelperImpl.java
+++ src/main/java/de/mxro/service/jre/internal/ShutdownHelperImpl.java
@@ -10,43 +10,64 @@ import de.mxro.service.utils.ShutdownHelper;
 public class ShutdownHelperImpl implements ShutdownHelper {
 
 	private final ActivityMonitor activityMonitor;
-	
+
 	private final AtomicInteger shutdownAttempts;
 	private final AtomicBoolean isShutdown;
 	private final AtomicBoolean isShuttingDown;
+
+	private final static int DEFAULT_DELAY = 10;
+	private final static int MAX_ATTEMPTS = 300;
 	
 	@Override
 	public boolean isShutdown() {
-		// TODO Auto-generated method stub
-		return false;
+		return isShutdown.get();
 	}
 
 	@Override
 	public boolean isShuttingDown() {
-		// TODO Auto-generated method stub
-		return false;
+		return isShuttingDown.get();
 	}
 
 	@Override
-	public void shutdown(SimpleCallback callback) {
+	public void shutdown(final SimpleCallback callback) {
 		assert !this.isShutdown() && !this.isShuttingDown();
-		
+
 		this.isShuttingDown.set(true);
-		
+
 		if (activityMonitor.pendingOperations() == 0) {
-			
+			this.isShutdown.set(true);
+			callback.onSuccess();
+
 			return;
 		}
-		
+
+		new Thread() {
+
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(DEFAULT_DELAY);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+				int attempts = shutdownAttempts.incrementAndGet();
+				
+				
+				shutdown(callback);
+			}
+			
+			
+			
+		}.start();
 	}
 
-	public ShutdownHelperImpl() {
+	public ShutdownHelperImpl(ActivityMonitor activityMonitor) {
 		super();
+		this.activityMonitor = activityMonitor;
+
 		this.shutdownAttempts = new AtomicInteger(0);
 		this.isShutdown = new AtomicBoolean(false);
 		this.isShuttingDown = new AtomicBoolean(false);
 	}
 
-	
-	
 }
