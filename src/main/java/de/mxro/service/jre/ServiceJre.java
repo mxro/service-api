94d82f8f3ce8f7bd6e11b3274610126ffb427c14
mxro <mxro@nowhere.com>
2014-09-19 15:05:43 +1000

    Making ShutdownHelper and counter safe for use in GWT apps Autocommit Fri Sep 19 15:05:43 EST 2014

--- src/main/java/de/mxro/service/jre/ServiceJre.java
+++ src/main/java/de/mxro/service/jre/ServiceJre.java
@@ -1,5 +1,6 @@
 package de.mxro.service.jre;
 
+import one.utils.jre.concurrent.JreConcurrency;
 import de.mxro.service.internal.OperationCounterImpl;
 import de.mxro.service.internal.ShutdownHelperImpl;
 import de.mxro.service.utils.OperationCounter;
@@ -7,25 +8,26 @@ import de.mxro.service.utils.ShutdownHelper;
 
 public class ServiceJre {
 
-	/**
-	 * <p>A simple counter for how many operations a service is processing at any
-	 * point in time.
-	 * 
-	 * @return A new operation counter instance.
-	 */
-	public static final OperationCounter createOperationCounter() {
-		return new OperationCounterImpl();
-	}
+    /**
+     * <p>
+     * A simple counter for how many operations a service is processing at any
+     * point in time.
+     * 
+     * @return A new operation counter instance.
+     */
+    public static final OperationCounter createOperationCounter() {
+        return new OperationCounterImpl(new JreConcurrency());
+    }
 
-	/**
-	 * <p>A helper to make shutdown operations safer.
-	 * 
-	 * @param operationCounter
-	 * @return
-	 */
-	public static final ShutdownHelper createShutdownHelper(
-			OperationCounter operationCounter) {
-		return new ShutdownHelperImpl(operationCounter);
-	}
+    /**
+     * <p>
+     * A helper to make shutdown operations safer.
+     * 
+     * @param operationCounter
+     * @return
+     */
+    public static final ShutdownHelper createShutdownHelper(final OperationCounter operationCounter) {
+        return new ShutdownHelperImpl(operationCounter);
+    }
 
 }
